3b729ac7dcbc795b30e5e8404fc0f4e00e2267b0
Oana-Florentina <floryflorentina55@gmail.com>
2023-10-07 17:38:15 +0300

    Create ex10.py

---
+++ lab1/ex10.py
@@ -0,0 +1,21 @@
+#Write a function that counts how many words exists in a text.
+# A text is considered to be form out of words that are separated 
+#by only ONE space. For example: "I have Python exam" has 4 words.
+
+def count_spaces(text):
+    word_count=0
+    if text[0]== ' ':
+        word_count=word_count-1
+    n=len(text)
+    if text[n-1]== ' ':
+        word_count=word_count-1
+    for char in text:
+        if char == ' ':
+            word_count=word_count+1
+    return word_count+1
+
+    
+text = " I have Python exam "
+word_count= count_spaces(text)
+
+print(f"The text contains {word_count} words")
