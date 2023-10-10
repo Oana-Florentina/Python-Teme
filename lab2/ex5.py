30d95a833a8a176026939fcbf87c4dc590adb8fe
Oana-Florentina <floryflorentina55@gmail.com>
2023-10-10 11:10:04 +0300

    ex cu matrice

---
+++ lab2/ex5.py
@@ -0,0 +1,18 @@
+#5. Write a function that receives as parameter a matrix and will return
+# the matrix obtained by replacing all the elements under the main diagonal with 0 (zero).
+def matrixReplace(A,n):
+    for i in range (0,n):
+        for j in range (0,i):
+            A[i][j]=0
+    return A
+matrix = [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+    ]
+result_matrix = matrixReplace(matrix, 4)
+
+# Print the matrix row by row
+for row in result_matrix:
+    print(row)
